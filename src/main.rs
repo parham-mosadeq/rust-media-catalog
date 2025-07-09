3aa4037548e50e6caea0947563e69bb750779411
= <pmosadeqzadeh@gmail.com>
2025-07-09 19:03:07 +0330

    chore: added pattern matching

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,25 @@ fn print_media(media: Media) {
 
 impl Media {
     fn description(&self) -> String {
-        if let Media::Book { title, author } = self {
-            format!("Book desc is: {title} and {author}")
-        } else if let Media::Movie { title, director } = self {
-            format!("Movie desc is: {title} and {director}")
-        } else if let Media::Audiobook { title } = self {
-            format!("Audiobook desc is: {title}  ")
-        } else {
-            String::from("Media Desc")
+        // if let Media::Book { title, author } = self {
+        //     format!("Book desc is: {title} and {author}")
+        // } else if let Media::Movie { title, director } = self {
+        //     format!("Movie desc is: {title} and {director}")
+        // } else if let Media::Audiobook { title } = self {
+        //     format!("Audiobook desc is: {title}  ")
+        // } else {
+        //     String::from("Media Desc")
+        // }
+
+        // * Two ways to write match
+        match self {
+            Media::Book { title, author } => format!("Book desc is: {title} and {author}"),
+            Media::Audiobook { title } => {
+                format!("Audiobook desc is: {title}  ")
+            }
+            Media::Movie { title, director } => {
+                format!("Movie desc is: {title} and {director}")
+            }
         }
     }
 }
