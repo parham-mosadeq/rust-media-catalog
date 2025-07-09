d20e13539c6d04dffac06a826db537dc8ef7631f
= <pmosadeqzadeh@gmail.com>
2025-07-09 19:00:56 +0330

    chore: adding desc to all medias

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,20 @@ fn print_media(media: Media) {
     println!("{:#?}", media);
 }
 
+impl Media {
+    fn description(&self) -> String {
+        if let Media::Book { title, author } = self {
+            format!("Book desc is: {title} and {author}")
+        } else if let Media::Movie { title, director } = self {
+            format!("Movie desc is: {title} and {director}")
+        } else if let Media::Audiobook { title } = self {
+            format!("Audiobook desc is: {title}  ")
+        } else {
+            String::from("Media Desc")
+        }
+    }
+}
+
 fn main() {
     let book = Media::Book {
         title: "TestBookTitle".to_string(),
@@ -24,6 +38,9 @@ fn main() {
         title: String::from("someRandomAudioBook"),
     };
 
+    println!("{}", audio_book.description());
+    println!("{}", movie.description());
+    println!("{}", book.description());
     print_media(book);
     print_media(movie);
     print_media(audio_book);
