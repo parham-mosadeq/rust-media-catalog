48a39f7ac3147a1a3f8bed4b219fb4d3c0fe2ccf
= <pmosadeqzadeh@gmail.com>
2025-07-09 18:46:46 +0330

    chore: created enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
+}
+
+fn print_media(media: Media) {
+    println!("{:#?}", media);
+}
+
 fn main() {
     println!("Hello, world!");
 }
